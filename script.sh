969b6ddb8e2a38be5ccfb72fa7ea06993aebbbf9
Dejan <55488175+dejandro@users.noreply.github.com>
2023-10-20 15:15:34 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,13 +8,13 @@ sudo rm /etc/apt/sources.list.d/cuda*
 sudo apt-get autoremove && sudo apt-get autoclean -y
 sudo rm -rf /usr/local/cuda*
 
-sudo apt-get update
+sudo apt-get update -y
 
 ### install other import packages
-sudo apt-get install g++ freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
+sudo apt-get install g++ freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev -y
 
 ### Security Update
 sudo apt-get -s dist-upgrade | grep "^Inst" | grep -i securi | awk -F " " {'print $2'} | xargs sudo apt-get install -y
 
 ### install Nvidia Driver
-sudo apt install nvidia-driver-525
+sudo apt install nvidia-driver-525 -y
